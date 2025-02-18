26a0a7d6fa2a6c73abf8d4f3a03b03c12192894a
whynotBb <whynot.byoul@gmail.com>
2025-02-18 13:02:53 +0900

    배너

--- src/pages/HomePage/components/Banner/Banner.jsx
+++ src/pages/HomePage/components/Banner/Banner.jsx
@@ -1,10 +1,22 @@
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
+import "./Banner.style.css";
 
 const Banner = () => {
-	const { data } = usePopularMoviesQuery();
+	const { data, isLoading, isError, error } = usePopularMoviesQuery();
 	console.log(data);
-
-	return <div>Banner</div>;
+	if (isLoading) {
+		return <h1>loading</h1>;
+	}
+	if (isError) {
+		return <h1>{error.message}</h1>;
+	}
+	return (
+		<div className="banner" style={{ backgroundImage: `url(https://media.themoviedb.org/t/p/w533_and_h300_bestv2/${data.results[0].backdrop_path})` }}>
+			<div>
+				<h1>{data.results[0].title}</h1>
+				<p>{data.results[0].overview}</p>
+			</div>
+		</div>
+	);
 };
-
 export default Banner;
